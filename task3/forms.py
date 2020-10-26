e100bd3f4b3a57b0f674829e944b99b74ac654e6
conor fennell <fennelco@tcd.ie>
2020-10-26 22:11:46 +0000

    updated settings

--- task3/forms.py
+++ task3/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .task2 import reverseIterative
+
 class inputForm(forms.Form):
     #Just going to have 1 input, the string we want to reverse
     string = forms.CharField(label='Please enter a string to be reversed')
